392cf359368984704a69554acaa90cb3d9324eb6
ayh9601 <ays9499@naver.com>
2022-02-06 06:13:49 +0900

    stage7

--- js/weather.js
+++ js/weather.js
@@ -1,11 +1,22 @@
 function onGeoOk(position){
     const lat = position.coords.latitude;
     const lng = position.coords.longitude
-    console.log("You live in", lat, lng);
+    const API = "313d868ff36db4b35da0d3345b7359dc"
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API}&units=metric`;
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        const weather = document.querySelector("#weather span:first-child");
+        const temp = document.querySelector("#weather span:nth-of-type(2)");
+        const city = document.querySelector("#weather span:last-child");
+        city.innerText = data.name;
+        temp.innerText = data.main.temp;
+        weather.innerText = `${data.weather[0].main}`;
+    });
 }
 
 function onGeoError(){
-    alert("Location Error! I think You are Inversion");
+    alert("Location Error! I think You are Inversioned");
 }
 
 navigator.geolocation.getCurrentPosition(onGeoOk,onGeoError);
